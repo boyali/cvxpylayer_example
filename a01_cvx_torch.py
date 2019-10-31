261a4d23dd4f005cc0747019f745f0ae57a5ac13
ali <boyali@gmail.com>
2019-10-31 12:27:32 +0900

    first commit

---
+++ a01_cvx_torch.py
@@ -0,0 +1,24 @@
+import cvxpy as cp
+import torch
+from cvxpylayers.torch import CvxpyLayer
+
+n, m = 2, 3
+x = cp.Variable(n)
+A = cp.Parameter((m, n))
+b = cp.Parameter(m)
+constraints = [x >= 0]
+objective = cp.Minimize(0.5 * cp.pnorm(A @ x - b, p=1))
+problem = cp.Problem(objective, constraints)
+assert problem.is_dpp()
+
+cvxpylayer = CvxpyLayer(problem, parameters=[A, b], variables=[x])
+A_tch = torch.randn(m, n, requires_grad=True)
+b_tch = torch.randn(m, requires_grad=True)
+
+# solve the problem
+solution, = cvxpylayer(A_tch, b_tch)
+
+# compute the gradient of the sum of the solution with respect to A, b
+grad = solution.sum().backward()
+
+print(grad)
